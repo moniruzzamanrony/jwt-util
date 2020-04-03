1bf9c47fa10e684f4368dc81d04c58d536ac2477
monirozzamanroni <monirozzaman35-223@diu.edu.bd>
2020-04-03 22:15:03 +0600

    Enable Faign

---
+++ src/main/java/com/itvillage/App.java
@@ -0,0 +1,17 @@
+package com.itvillage;
+
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+/**
+ * Hello world!
+ *
+ */
+@EnableFeignClients
+public class App 
+{
+    public static void main( String[] args )
+    {
+
+        //System.out.println(feignUtil.login("roni2923","roni2923") );
+    }
+}
